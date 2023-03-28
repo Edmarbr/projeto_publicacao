9e435429e1fc89e864b44e6e3304f6a52b60de24
Edmarbr <111692243+Edmarbr@users.noreply.github.com>
2023-03-27 21:44:26 -0300

    update

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -6,16 +6,16 @@
         if (isset($_POST["form_nome"]) && isset($_POST["form_email"]) && isset($_POST["form_senha"])) {
                 $nome = $_POST["form_nome"];
                 $email = $_POST["form_email"];
-                $senha = $_POST["form_senha"];
+                $senha = password_hash($_POST["form_senha"], PASSWORD_DEFAULT);
                 
                 $numeroUsu = rand(1000000, 10000000);
                 $_SESSION["codigo"] = $numeroUsu;
                 $_SESSION["nome"] = $nome;
 
-                $verificarEmail_Senha = mysqli_query($conectarBD, "SELECT email, senha FROM cadastro WHERE email = '$email' OR senha = '$senha'");
-                $linhasRetornadas = mysqli_affected_rows($conectarBD);
+                $verificarEmail_Senha = mysqli_query($conectarBD, "SELECT email, senha FROM cadastro WHERE email = '$email'");
+                $Retornosql = mysqli_fetch_row($verificarEmail_Senha);
 
-                if ($linhasRetornadas > 0) {                            // verificação se existe um email ou senha já registrado no banco de dados
+                if ($Retornosql[0] == $email) {                            // verificação se existe um email ou senha já registrado no banco de dados
                         header("Location: cadastro.html");
                 } else {
                         mysqli_query($conectarBD, "INSERT INTO cadastro (nome, email, senha) VALUES ('$nome', '$email', '$senha')");
--- login/login.php
+++ login/login.php
@@ -7,10 +7,10 @@
         $email = $_POST["form_email"];
         $senha = $_POST["form_senha"];
 
-        $Sql = mysqli_query($conectarBD, "SELECT email, nome, senha FROM cadastro WHERE email = '$email' AND senha = '$senha'");
+        $Sql = mysqli_query($conectarBD, "SELECT email, nome, senha FROM cadastro WHERE email = '$email' LIMIT 1");
         $linhas_afetadas = mysqli_affected_rows($conectarBD);
 
-        if ($linhas_afetadas > 0) {
+        if ($linhas_afetadas > 0 && password_verify($senha, mysqli_fetch_row($Sql)[2])) {
             $numeroUsu = rand(1000000, 10000000);
             $_SESSION["codigo"] = $numeroUsu;
             $_SESSION["nome"] = mysqli_fetch_row($Sql)[1];
@@ -20,6 +20,5 @@
             exit;
         }
     }
-
     mysqli_close($conectarBD);
 ?>
